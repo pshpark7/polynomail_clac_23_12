f39dba6c7d1a0603aba15ff33c181e61cf70c038
pshpark7 <pshpark07@gmail.com>
2023-12-27 09:47:14 +0900

    10 + 20 + 30

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,21 +2,32 @@ package com.ll;
 
 public class Calc {
   public static int run(String exp) {
-    if (exp.contains("+")) {
-      String[] bits = exp.split(" \\+ ");
 
-      int a = Integer.parseInt(bits[0]);
-      int b = Integer.parseInt(bits[1]);
-      return a + b;
-    } else if (exp.contains("-")) {
-      String[] bits = exp.split(" \\- ");
-      int a = Integer.parseInt(bits[0]);
-      int b = Integer.parseInt(bits[1]);
-      return a - b;
-    } else
-  return 0;
+    boolean needToPlus = exp.contains("+");
+    boolean needToMinus = exp.contains("-");
 
+    String[] bits = null;
 
+    if (needToPlus) {
+      bits = exp.split(" \\+ ");
+    } else if (needToMinus) {
+      bits = exp.split(" - ");
+    }
 
+    int a = Integer.parseInt(bits[0]);
+    int b = Integer.parseInt(bits[1]);
+    int c = 0;
+
+    if (bits.length > 2) {
+      c = Integer.parseInt(bits[2]);
+    }
+
+    if (needToPlus) {
+      return a + b + c;
+    } else if (needToMinus) {
+      return a - b - c;
+    }
+
+    throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
   }
 }
