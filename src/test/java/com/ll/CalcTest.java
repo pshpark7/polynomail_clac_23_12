6093d7115c149d9ffb1437e19230fe1aec2f88ed
pshpark7 <pshpark07@gmail.com>
2023-12-27 11:25:37 +0900

    10 - 10 - 10 - 10

--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -57,5 +57,23 @@ public class CalcTest {
 
     assertThat(Calc.run("10 - 20 + 30")).isEqualTo(20);
   }
+
+  @Test
+  @DisplayName("10 - 10 - 10 - 10 == -20")
+  void t9() {
+    assertThat(Calc.run("10 - 10 - 10 - 10")).isEqualTo(-20);
+  }
+
+  @Test
+  @DisplayName("10 - 10 - 10 - 10 + 10 + 10 - 10 == -10")
+  void t10() {
+    assertThat(Calc.run("10 - 10 - 10 - 10 + 10 + 10 - 10")).isEqualTo(-10);
+  }
+
+  @Test
+  @DisplayName("10 * 10 == 100")
+  void t11() {
+    assertThat(Calc.run("10 * 10")).isEqualTo(100);
+  }
 }
 
